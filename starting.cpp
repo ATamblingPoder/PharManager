7c329ff4dceb36ae369eddf1adfba1d2749a3af7
ATamblingPoder <117522662+ATamblingPoder@users.noreply.github.com>
2023-05-07 11:59:39 +0530

    Starting Code, will change later

---
+++ starting.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+using namespace std;
+
+int addRecords(){
+	int aR_number;
+	cout << "Number of records to add: ";
+	cin >> aR_number;
+	for(int aR_i = 0; aR_i < aR_number; aR_i++){
+		cout << "Enter Name of Medicine: ";
+		string aR_name, aR_rack;
+		int aR_quantity
+		getline(cin, aR_name);
+
+	}
+}
